59cc68e0b8c0995de4703f60e3d62799e828c01d
qinwang <qinwang@sobey.com>
2017-08-13 16:44:40 +0800

    处理时间格式，生成图片

--- tools/ConnectDB.py
+++ tools/ConnectDB.py
@@ -1,24 +1,39 @@
 #coding=utf-8
 import psycopg2
+import datetime
+import time
 
 class DbSomething():
-    def __init__(self,url,database,username, password,port=5432):
-        self.url = url
+    def __init__(self,ip,database,username, password,port=5432):
+        self.ip = ip
         self.database = database
         self.username = username
         self.password = password
         self.port = port
-'''如果把connection写到__init__中，每次初始化类就好？可以一试'''
+    '''如果把connection写到__init__中，每次初始化类就好？可以一试'''
     def connection(self):
-        conn = psycopg2.connect(self.database, self.username, self.password, self.port)
+        conn = psycopg2.connect(host=self.ip,
+                                database=self.database,
+                                user=self.username,
+                                password=self.password,
+                                port=self.port)
         cur = conn.cursor()
-        return cur,conn
+        return cur, conn
+
+    '''创建表操作'''
+    def creatTable(self, sql):
+        cur, conn = self.connection()
+        cur.execute(sql)
+        print ("table is created")
+        conn.commit()
+        cur.close()
+        conn.close()
 
     '''插入数据
     #table，which table insert to
     #data,what to insert
     '''
-    def Insert(self,table, data):
+    def Insert(self,sql):
         pass
 
     '''
@@ -26,10 +41,25 @@ class DbSomething():
     #table
     #conditions
     '''
-    def search(self,table, conditons):
-        pass
+    def search(self):
+        v1 =[]
+        v2=[]
+        '''sql = select ctime ,count(*)
+                from qps
+                group by ctime order by ctime
+              '''
+        sql = 'select ctime ,count(*) from qps group by ctime order by ctime'
+        cur, conn = self.connection()
+        cur.execute(sql)
+        data = cur.fetchall()
+        #data list type
 
-    #关闭数据库连接
-    def closeDb(self,cur,conn):
+        for i in data:
+            date = datetime.datetime.strftime(i[0], '%Y-%m-%d %H:%M:%S')
+            print (type(date),type(i[1]))
+            v1.append(date)
+
+            v2.append(i[1])
         cur.close()
-        conn.cloase()
+        conn.close()
+        return v1,v2
--- tools/GenPic.py
+++ tools/GenPic.py
@@ -4,6 +4,22 @@
 使用pyecharts 创建不同的图形图像
 目前有：xxxx等图像
 '''
-import pyecharts
+from  pyecharts import Line
+from  tools.ConnectDB import DbSomething
+
+a = DbSomething('localhost','dns_query', 'postgres', 111111)
+v1,v2 = a.search()
+print (v1,v2)
 
 class GenPic:
+    '''生成折线图'''
+    def lineChart(self):
+        line= Line("QPS图")
+        line.add('', v1, v2)
+        line.show_config()
+        line.render()
+
+
+if __name__=='__main__':
+    pic = GenPic()
+    pic.lineChart()
